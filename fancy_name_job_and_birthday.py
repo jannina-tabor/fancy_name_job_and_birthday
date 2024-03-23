54af9f271d8596c7039a027d43a2ebc86719eb5b
Jannina <jaitabor2033@gmail.com>
2024-03-23 19:48:54 +0800

    Created a Pseudocode

---
+++ fancy_name_job_and_birthday.py
@@ -0,0 +1,9 @@
+# pseudocode
+
+# ask the user to input their name
+# ask the user to input their dream job
+# ask the user to input their birthday
+# print the inputted name in a fancy way
+# print the inputted dream job in a fancy way
+# print the inputted birthday in a fancy way
+
