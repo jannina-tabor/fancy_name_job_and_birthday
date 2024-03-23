258c70f81d05f59cba55c7757b9197c1fb954ea7
Jannina <jaitabor2033@gmail.com>
2024-03-23 19:54:42 +0800

    Ask the user to input their dream job

--- fancy_name_job_and_birthday.py
+++ fancy_name_job_and_birthday.py
@@ -4,6 +4,8 @@
 name = input("Please enter your name: ")
 
 # ask the user to input their dream job
+dream_job = input("Please enter your dream job: ")
+
 # ask the user to input their birthday
 # print the inputted name in a fancy way
 # print the inputted dream job in a fancy way
