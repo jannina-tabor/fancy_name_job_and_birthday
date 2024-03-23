a27ee7c0d63b52c2d8d03703b5c287cc5b3bc396
Jannina <jaitabor2033@gmail.com>
2024-03-23 20:11:27 +0800

    Changed the font style in name

--- fancy_name_job_and_birthday.py
+++ fancy_name_job_and_birthday.py
@@ -12,7 +12,7 @@ dream_job = input("Please enter your dream job: ")
 birthday = input("Please enter your date of birth: ")
 
 # print the inputted name in a fancy way
-font_name = pyfiglet.Figlet(font='dancing_font')
+font_name = pyfiglet.Figlet(font='js_block_letters')
 color_name = '\033[0;35m'
 fancy_name = f"{color_name}{font_name.renderText(name)}"
 print (fancy_name)
